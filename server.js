4cad985463016284b9c4388a1c9d6df7c4ec54ea
BrantBart <brant.barton64@gmail.com>
2024-03-14 21:58:05 -0600

    week 10 personal assignment.

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts");
 const env = require("dotenv").config();
 const app = express();
 const static = require("./routes/static");
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute");
 
 /* ***********************
  * View Engine and Templates
@@ -26,9 +28,9 @@ app.use(static);
 /* ***********************
  * Index Route
  *************************/
-app.get("/", function (req, res) {
-  res.render("index", { title: "Home" });
-});
+app.use("/inv", inventoryRoute);
+
+app.get("/", baseController.buildHome);
 
 /* ***********************
  * Local Server Information
