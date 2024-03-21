724290bb49fc5b14a5d48d80b79943f1ccc00718
BrantBart <brant.barton64@gmail.com>
2024-03-20 22:00:44 -0600

    trying to fix connection

--- database/index.js
+++ database/index.js
@@ -1,22 +1,26 @@
 const { Pool } = require("pg");
 require("dotenv").config();
+
 /* ***************
  * Connection Pool
  * SSL Object needed for local testing of app
  * But will cause problems in production environment
  * If - else will make determination which to use
  * *************** */
+
 let pool;
-if (process.env.NODE_ENV == "development") {
+
+if (process.env.NODE_ENV === "development") {
+  // For development environment
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    // SSL configuration - disable SSL/TLS for local development
     ssl: {
-      rejectUnauthorized: false,
+      rejectUnauthorized: false, // Disables SSL certificate verification
     },
   });
 
-  // Added for troubleshooting queries
-  // during development
+  // Added for troubleshooting queries during development
   module.exports = {
     async query(text, params) {
       try {
@@ -30,8 +34,12 @@ if (process.env.NODE_ENV == "development") {
     },
   };
 } else {
+  // For production environment and other environments
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    // No SSL configuration provided, relying on default SSL/TLS behavior
   });
+
+  // Exporting the pool for use in other modules
   module.exports = pool;
 }
